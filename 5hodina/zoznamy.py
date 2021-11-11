bf5d91b170cda3c2aaf3f2017e1e751b960b7bc7
z21ite14 <z21ite14@z00p3-10.SPSKNM>
2021-11-11 13:24:43 +0100

    zoznamy

--- 5hodina/zoznamy.py
+++ 5hodina/zoznamy.py
@@ -3,11 +3,26 @@ zoznam_cisel = [1,5,8,9,40,21]
 zoznam_pismen = ['a','b']
 zoznam_mix = ["slovo", 14, 'a', 55]
 
+zoznam_cisel[0] = 9999
+print(zoznam_cisel[4])
+print(zoznam_pismen[1])
+print(zoznam_mix[-1])
+
 print(zoznam_cisel)
 print(zoznam_pismen)
 print(zoznam_mix)
+print("------------")
 
 zoznam = list()
 print(zoznam)
 zoznam_range = list(range(3, 7))
-print()
+print(zoznam_range)
+
+#NEOREZANY zoznam
+neorezany_zoznam = list(range(10))
+print(neorezany_zoznam)
+
+print(neorezany_zoznam[0:5])
+print(neorezany_zoznam[2:8])
+print(neorezany_zoznam[1:7:2])
+print(neorezany_zoznam[2:9:3])
